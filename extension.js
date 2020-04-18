4c6436089da23551697aeb86025948935a1c3ea4
Youssef Saade <youssefsaade95@gmail.com>
2020-04-18 17:50:31 +0200

    Change code.

--- extension.js
+++ extension.js
@@ -1,118 +1,92 @@
-"use strict";
-
-const Lang = imports.lang;
 const St = imports.gi.St;
-const Gio = imports.gi.Gio;
-const Main = imports.ui.main;
-const PanelMenu = imports.ui.panelMenu;
-const Atk = imports.gi.Atk;
-// const Mainloop = imports.mainloop;
-// const Shell = imports.gi.Shell;
-// const MessageTray = imports.ui.messageTray;
-// const Config = imports.misc.config;
-
-const SHOW_INDICATOR_KEY = "show-indicator";
-const TO_DARK = "dark-gray";
-const TO_LIGHT = "light-gray";
-
 const Me = imports.misc.extensionUtils.getCurrentExtension();
-const Convenience = Me.imports.convenience;
+const Main = imports.ui.main;
 
 const execSync = require("child_process").execSync;
-// import { execSync } from "child_process";
 
-// const dark_mode = execSync(
-//     "gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'"
-// );
-// const light_mode = execSync(
-//     "gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'"
-// );
-
-const IndicatorName = "ThemeSwitcher";
+// const IndicatorName = "ThemeSwitcher";
 const DarkIcon = "dark-gray";
 const LightIcon = "light-gray";
 // const DarkIcon = "dark-black";
 // const LightIcon = "light-black";
 
-// const ThemeSwitcher = new Lang.Class({
-//     Name: IndicatorName,
-//     Extends: PanelMenu.Button,
-class ThemeSwitcher extends PanelMenu.Button {
-    constructor() {
-        // this.parent(null, IndicatorName);
-        this.actor.accessible_role = Atk.Role.TOGGLE_BUTTON;
+let button, icon;
 
-        this._settings = Convenience.getSettings();
-        this._settings.connect(
-            "changed::" + SHOW_INDICATOR_KEY,
-            Lang.bind(this, function () {
-                if (this._settings.get_boolean(SHOW_INDICATOR_KEY))
-                    this.actor.show();
-                else this.actor.hide();
-            })
-        );
-        if (!this._settings.get_boolean(SHOW_INDICATOR_KEY)) {
-            this.actor.hide();
-        }
+function set_dark() {
+    execSync(
+        "gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-maia-dark'"
+    );
+    execSync(
+        "dconf write /org/gnome/shell/extensions/user-theme/name \"'Adwaita-maia-dark'\""
+    );
+    Main.notify(_("Switched to Dark Theme"));
+}
 
-        this._icon = new St.Icon({
-            style_class: "system-status-icon",
-        });
-        this._icon.gicon = Gio.Icon.new_for_string(
-            Me.path + "/icons/" + LightIcon + ".svg"
-        );
+function set_light() {
+    execSync(
+        "gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-maia'"
+    );
+    execSync(
+        "dconf write /org/gnome/shell/extensions/user-theme/name \"'Adwaita-maia'\""
+    );
+    Main.notify(_("Switched to Light Theme"));
+}
 
-        this._state = false; // light theme
-
-        this.actor.add_actor(this._icon);
-        this.actor.add_style_class_name("panel_status_button");
-        this.actor.connect(
-            "button-press-event",
-            Lang.bind(this, this.toggleState)
-        );
+function toggle_theme() {
+    let variant = execSync(
+        "gsettings get org.gnome.desktop.interface gtk-theme"
+    )
+        .toString("UTF-8")
+        .split("'")[1];
+    if (variant.includes("dark")) {
+        set_light();
+        return false;
+    } else {
+        set_dark();
+        return true;
     }
+}
 
-    toggleState() {
-        if (this._state) {
-            // set light theme
-            this._state = false;
-            execSync(
-                "gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-maia'"
-            );
-            execSync(
-                "dconf write /org/gnome/shell/extensions/user-theme/name \"'Adwaita-maia'\""
-            );
-            this._icon.gicon = Gio.icon_new_for_string(
-                Me.path + "/icons/" + LightIcon + ".png"
-            );
-            Main.notify(_("Switched to Light Theme"));
+function init() {
+    button = new St.Bin({
+        style_class: "panel-button",
+        reactive: true,
+        can_focus: true,
+        x_fill: true,
+        y_fill: false,
+        track_hover: true,
+    });
+
+    var today = new Date();
+    time = today.getHours() + ":" + today.getMinutes();
+    var sunrise = execSync("hdate -s | grep rise | cut -f 2 -d ' '")
+        .toString("UTF-8")
+        .slice(0, 5);
+    var sunset = execSync("hdate -s | grep set | cut -f 2 -d ' '")
+        .toString("UTF-8")
+        .slice(0, 5);
+    if (sunrise < time && time < sunset) {
+        icon = new St.Icon({ style_class: LightIcon });
+    } else {
+        icon = new St.Icon({ style_class: DarkIcon });
+    }
+    button.set_child(icon);
+
+    button.connect("button-press-event", function () {
+        if (toggle_theme()) {
+            icon = new St.Icon({ style_class: DarkIcon });
         } else {
-            // set dark theme
-            this._state = true;
-            execSync(
-                "gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-maia-dark'"
-            );
-            execSync(
-                "dconf write /org/gnome/shell/extensions/user-theme/name \"'Adwaita-maia-dark'\""
-            );
-            this._icon.gicon = Gio.icon_new_for_string(
-                Me.path + "/icons/" + DarkIcon + ".png"
-            );
-            Main.notify(_("Switched to Dark Theme"));
+            icon = new St.Icon({ style_class: LightIcon });
         }
-    }
-
-    destroy() {
-        this.parent();
-    }
+        button.set_child(icon);
+    });
 }
 
 function enable() {
-    ThemeSwitcher = new ThemeSwitcher();
-    Main.panel.addToStatusArea(IndicatorName, ThemeSwitcher);
+    // init();
+    Main.panel._rightBox.insert_child_at_index(button, 0);
 }
 
 function disable() {
-    ThemeSwitcher.destroy();
-    ThemeSwitcher = null;
+    Main.panel._rightBox.remove_child(button);
 }
